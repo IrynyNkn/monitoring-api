73f852e62bc03c6b2c20c8b831a7247d05f0bc6a
Iryna Nakonechna <iryninak03@gmail.com>
2024-04-07 19:17:30 +0300

    added ping update and cancel

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -1,5 +1,6 @@
-import time
-from celery import Celery
+import json
+import redis
+from celery import Celery, signals
 from icmplib import ping
 
 
@@ -7,33 +8,53 @@ celery = Celery(__name__)
 celery.conf.broker_url = "redis://redis:6379/0"
 celery.conf.result_backend = "redis://redis:6379/0"
 
-
-@celery.task(name="create_task")
-def create_task(a, b, c):
-    time.sleep(a)
-    return b + c
+redis_client = None
 
 
-@celery.task(name="continuous_ping_pro")
-def continuous_ping_pro(host, interval):
-    response = ping(host, count=1)
-    if response.packets_received > 0:
-        print(f'Ping to {host} succeeded.')
-    else:
-        print(f'Ping to {host} failed.')
-
-    continuous_ping_pro.apply_async(args=[host, interval], countdown=interval)
+@signals.worker_process_init.connect
+def setup_redis(**kwargs):
+    print('connected celery to redis')
+    global redis_client
+    redis_client = redis.Redis(host='redis', port=6379, decode_responses=True, db=1)
 
 
 @celery.task(name="continuous_ping")
-def continuous_ping(host, interval):
-    while True:
-        response = ping(host, count=1)
-        if response.packets_received > 0:
-            print(f'Ping to {host} succeeded.')
-        else:
-            print(f'Ping to {host} failed.')
+def continuous_ping(host, interval, task_id):
+    global redis_client
+    ping_config_json = redis_client.get(task_id) if redis_client is not None else None
 
-        time.sleep(interval)
+    print('TASK ID', task_id)
+    if ping_config_json is not None:
+        ping_config = json.loads(ping_config_json)
+        if ping_config['status'] == 'active':
+            response = ping(host, count=1)
+            if response.packets_received > 0:
+                print(f'Ping to {host} succeeded.')
+            else:
+                print(f'Ping to {host} failed.')
+
+            continuous_ping.apply_async(args=[host, ping_config['interval'], task_id], countdown=interval)
+        else:
+            print(f'Ping to {host} was canceled.')
+
+
+# @celery.task(name="create_task")
+# def create_task():
+#     global redis_client
+#     print('REDIS CON:', redis_client)
+#     time.sleep(1)
+#     return 2 + 2
+
+
+# @celery.task(name="continuous_ping")
+# def continuous_ping(host, interval):
+#     while True:
+#         response = ping(host, count=1)
+#         if response.packets_received > 0:
+#             print(f'Ping to {host} succeeded.')
+#         else:
+#             print(f'Ping to {host} failed.')
+#
+#         time.sleep(interval)
 
 
--- app/routes/icmp_ping.py
+++ app/routes/icmp_ping.py
@@ -1,50 +1,77 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
-from app.celery_worker import continuous_ping, continuous_ping_pro, celery
+from pydantic import BaseModel
+from uuid import uuid4
+import json
+from app.celery_worker import continuous_ping
+
+
+class UpdatePing(BaseModel):
+    interval: int
+
 
 router = APIRouter()
 
 
-@router.get("/icmp-ping/test")
-async def test():
-    return {"message": "Success"}
-
-
-@router.get("/icmp-ping/get-example")
-async def get_example(request: Request):
-    redis = request.app.state.redis_client
-    value = await redis.get("my-key")
-    return {"message": "Value set successfully", "value": value}
-
-
-@router.get("/icmp-ping/set-example")
-async def set_example(request: Request):
-    redis = request.app.state.redis_client
-    await redis.set("my-key", "my-value")
-    return {"message": "Value set successfully"}
-
-
-# @app.post("/test-celery")
-# def test_celery(amount: str, a: str, b: str):
-#     task = create_task.delay(int(amount), int(a), int(b))
-#     return JSONResponse({"Task Result:": task.get()})
-
 @router.get("/icmp-ping")
-async def icmp_ping(host: str, interval: int):
-    task = continuous_ping.delay(host, interval)
+async def icmp_ping(host: str, interval: int, request: Request):
+    task_id = str(uuid4())
+    task_info = {
+        "host": host,
+        "interval": interval,
+        "status": "active"
+    }
+
+    redis = request.app.state.redis_client
+    await redis.set(task_id, json.dumps(task_info))
+
+    task = continuous_ping.delay(host, interval, task_id)
     return JSONResponse({"Task Result:": task.id})
 
 
-@router.get("/icmp-ping-pro")
-async def icmp_ping_pro(host: str, interval: int):
-    # task_id = str(uuid4())
-    # task_info = {"host": host, "interval": interval, "status": "scheduled"}
-    # await r.set(task_id, json.dumps(task_info))  # Store task info in Redis
-    task = continuous_ping_pro.delay(host, interval)
-    return JSONResponse({"Task Result:": task.id})
+@router.post("/icmp-ping/update-ping/{task_id}")
+async def update_ping_task(req_body: UpdatePing, task_id: str, request: Request):
+    redis = request.app.state.redis_client
+
+    ping_config_json = await redis.get(task_id)
+    ping_config = json.loads(ping_config_json)
+    ping_config['interval'] = req_body.interval
+    await redis.set(task_id, json.dumps(ping_config))
+
+    return JSONResponse({"status": "updated", "task_id": task_id})
 
 
-@router.delete("/cancel-ping/{task_id}")
-async def cancel_ping_task(task_id: str):
-    celery.control.revoke(task_id, terminate=True)
+@router.delete("/icmp-ping/cancel-ping/{task_id}")
+async def cancel_ping_task(task_id: str, request: Request):
+    await request.app.state.redis_client.delete(task_id)
+    # celery.control.revoke(task_id, terminate=True)
     return JSONResponse({"status": "cancelled", "task_id": task_id})
+
+
+# @router.get("/icmp-ping/get-example")
+# async def get_example(request: Request):
+#     redis = request.app.state.redis_client
+#     value = await redis.get("my-key")
+#     return {"message": "Value set successfully", "value": value}
+#
+#
+# @router.get("/icmp-ping/set-example")
+# async def set_example(request: Request):
+#     redis = request.app.state.redis_client
+#     await redis.set("my-key", "my-value")
+#     return {"message": "Value set successfully"}
+
+
+# @router.get("/icmp-ping/test")
+# async def test():
+#     return {"message": "Success"}
+
+# @router.get("/icmp-ping/test-celery")
+# def test_celery():
+#     task = create_task.delay()
+#     return JSONResponse({"Task Result:": "nice"})
+
+# @router.get("/icmp-ping")
+# async def icmp_ping(host: str, interval: int):
+#     task = continuous_ping.delay(host, interval)
+#     return JSONResponse({"Task Result:": task.id})
